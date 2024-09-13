f8728b68d749d10fd8ceafffa9c0ae4787a5bd30
unknown <thouillez.juliette@orange.fr>
2024-09-13 17:24:03 +0200

    import components

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,14 @@
 </v-container>
 </template>
 
+<script setup>
+import MovieDetail from './components/MovieDetail.vue'
+import MovieList from './components/MovieList.vue'
+import MoviePagination from './components/MoviePagination.vue'
+import MovieSearch from './components/MovieSearch.vue'
+
+</script>
+
 <style lang="scss">
 
 </style>
--- src/components/movieSearch.vue
+++ src/components/movieSearch.vue
@@ -5,6 +5,6 @@
 </template>
 <script>
 export default {
-    name: 'movieSearch',
+    name: 'MovieSearch',
 }
 </script>
